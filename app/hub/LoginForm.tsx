2ae7624ad1e1f8fbf66d6e4c2be27aa061a3777e
Paul3831 <paul.moulerot@ecole-isitech.fr>
2024-02-23 20:04:29 +0100

    ajout login + synchronisation firebase

--- app/hub/LoginForm.tsx
+++ app/hub/LoginForm.tsx
@@ -1,76 +1,107 @@
-// Fichier : login/page.tsx
-import React from 'react';
-import { MailIcon, LockClosedIcon } from '@heroicons/react/solid';
-import Test from '../test/page'; // Assurez-vous que le chemin est correct
-import { UNSTABLE_REVALIDATE_RENAME_ERROR } from 'next/dist/lib/constants';
 
-const LoginPage = () => {
+'use client';
+import Test from '../test/page';
+import React, { useState } from 'react';
+
+
+import { MailIcon, LockClosedIcon, CheckCircleIcon } from '@heroicons/react/solid';
+
+
+// Importez la fonction de connexion depuis Firebase Auth
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebaseConfig'; // Mettez à jour avec le chemin vers votre configuration Firebase
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Empêcher la soumission classique du formulaire
+    e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-    const username = formData.get('username');
-    const password = formData.get('password');
-  
+    setError(''); // Réinitialiser l'erreur à chaque soumission
     try {
-      const response = await fetch('http://localhost/login.php', {
-        method: 'POST',
-        body: JSON.stringify({
-          password: password,
-          username: username,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        alert('Connexion réussie');
-        // Vous pouvez ajouter ici une logique pour gérer la connexion réussie,
-        // par exemple en sauvegardant le jeton d'accès et en redirigeant l'utilisateur.
-      } else {
-        alert('Échec de la connexion');
-      }
+      await signInWithEmailAndPassword(auth, email, password);
+     setShowConfirmation(true);
+      console.log('Connexion réussie');
+      window.location.href = "/hub";
     } catch (error) {
-      console.error("Erreur lors de la connexion", error);
-      alert('Une erreur s\'est produite lors de la connexion');
+      if (error instanceof Error) {
+        setError(error.message); // Afficher le message d'erreur de Firebase
+      } else {
+        setError('Une erreur est survenue lors de la connexion.');
+      }
     }
   };
-  
+
   return (
     <>
-      <div className="fixed inset-0 bg-black bg-opacity-50 overflow-hidden z-0">
-        <div className="absolute inset-0 filter blur-lg">
-          <Test />
-        </div>
-      </div>
+    
 
-      <div className="flex items-center justify-center min-h-screen z-10">
-        <div className="px-8 py-6 text-left bg-gray-800 text-white shadow-lg" style={{ zIndex: 20 }}>
-          <h3 className="text-2xl font-bold text-center">Se connecter</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="mt-4">
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 bg-gray-700">
-                <MailIcon className="h-5 w-5 text-gray-300" />
-                <input className="pl-2 outline-none border-none bg-transparent text-white" type='username' name="username" placeholder="Nom d'utilisateur" required />
-              </div>
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl bg-gray-700">
-                <LockClosedIcon className="h-5 w-5 text-gray-300" />
-                <input className="pl-2 outline-none border-none bg-transparent text-white" type="password" name="password" placeholder="Mot de passe" required />
-              </div>
-              <div className="flex items-center justify-between mt-4">
-                <button className="px-6 py-2 leading-5 text-gray-900 transition-colors duration-200 transform bg-white rounded-md hover:bg-gray-300 focus:outline-none focus:bg-gray-300" type="submit">
-                  Connexion
-                </button>
-              </div>
-            </div>
-          </form>
+    {showConfirmation && (
+      <div className="fixed inset-0 bg-black bg-opacity-50 overflow-hidden z-50">
+        <div className="m-auto bg-white p-5 rounded-lg flex flex-col items-center">
+          <CheckCircleIcon className="h-16 w-16 text-green-500" />
+          <h3 className="mt-2 text-lg font-semibold">Inscription réussie!</h3>
+          <p>Redirection vers l'accueil...</p>
         </div>
       </div>
-    </>
+    )}
+
+
+<div className="fixed inset-0 bg-black bg-opacity-50 overflow-hidden z-0">
+      <div className="absolute inset-0 filter blur-lg">
+        <Test />
+      </div>
+    </div>
+
+    <div className="flex items-center justify-center min-h-screen z-10">
+      <div className="px-8 py-6 text-left bg-gray-800 text-white shadow-lg" style={{ zIndex: 20 }}>
+        <h3 className="text-2xl font-bold text-center">Se connecter</h3>
+        {error && <p className="text-red-500 text-xs">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="mt-4">
+            <label className="block" htmlFor="email">
+              <MailIcon className="h-5 w-5 text-gray-300" />
+              <input
+                className="form-input mt-1 block w-full pl-10"
+                type="email"
+                name="email"
+                placeholder="Adresse e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label className="block mt-4" htmlFor="password">
+              <LockClosedIcon className="h-5 w-5 text-gray-300" />
+              <input
+                className="form-input mt-1 block w-full pl-10"
+                type="password"
+                name="password"
+                placeholder="Mot de passe"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <div className="flex items-center justify-between mt-4">
+              <button
+                className="px-6 py-2 text-white bg-blue-600 hover:bg-blue-700 focus:outline-none rounded"
+                type="submit"
+              >
+                Connexion
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </>
   );
 };
 
-export default LoginPage;
+export default LoginForm;
+
+
