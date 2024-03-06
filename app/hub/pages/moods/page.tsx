b093905a2c04138663a0d8e0a416a65157fb6686
Ilyes <ilyes.aidoudi@hotmail.com>
2024-03-06 01:08:57 +0100

    humeur relié bdd

--- app/hub/pages/moods/page.tsx
+++ app/hub/pages/moods/page.tsx
@@ -1,29 +1,55 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../../../../components/Sidebar';
 import Link from 'next/link';
 import ChatPopup from '../../../../components/Chat';
 import { HeartIcon, EmojiHappyIcon, EmojiSadIcon, LightningBoltIcon } from '@heroicons/react/solid';
+import { db, auth } from '../../../firebaseConfig';
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 const emotions = [
-  { icon: HeartIcon, name: 'Amour', mood: 'love' },
-  { icon: EmojiHappyIcon, name: 'Joyeux', mood: 'happy' },
-  { icon: EmojiSadIcon, name: 'Triste', mood: 'sad' },
-  { icon: LightningBoltIcon, name: 'Énergique', mood: 'energetic' },
+  { icon: HeartIcon, name: 'Amour', mood: '❤️' },
+  { icon: EmojiHappyIcon, name: 'Joyeux', mood: '😀' },
+  { icon: EmojiSadIcon, name: 'Triste', mood: '😢' },
+  { icon: LightningBoltIcon, name: 'Énergique', mood: '⚡' },
 ];
 
 const HumeurPage: React.FC = () => {
-  const [selectedEmotion, setSelectedEmotion] = useState<string>('');
+  const [selectedEmotion, setSelectedEmotion] = useState<string | null>(null);
 
-  const handleEmotionSelect = (emotion: string) => {
-    setSelectedEmotion(emotion);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        const docRef = doc(db, "users", user.uid);
+        getDoc(docRef).then(docSnap => {
+          if (docSnap.exists()) {
+            setSelectedEmotion(docSnap.data().mood);
+          } else {
+            console.log("No such document!");
+          }
+        });
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleEmotionSelect = async (mood: string) => {
+    setSelectedEmotion(mood);
+
+    const user = auth.currentUser;
+    if (user) {
+      const userRef = doc(db, "users", user.uid);
+      await setDoc(userRef, { mood }, { merge: true });
+    }
   };
 
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col items-center">
-        <h1 className="text-2xl font-bold text-center mt-10">Comment vous sentez-vous aujourd-hui ?</h1>
+        <h1 className="text-2xl font-bold text-center mt-10">Comment vous sentez-vous aujourd'hui ?</h1>
         <div className="flex justify-center mt-5 flex-wrap">
           {emotions.map(({ icon: Icon, name, mood }) => (
             <button
