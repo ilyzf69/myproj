1004a2f997e8d958cb32a10619f5cc69aa34d5b5
Ilyes <ilyes.aidoudi@hotmail.com>
2024-06-27 14:56:33 +0200

    okok

--- app/ui/test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logoImage from '../../image/test.png';
-
-export default function Logo() {
-  return (
-    <img src={logoImage.src} alt="Logo" className="h-12 w-12" />
-  );
-}
-
